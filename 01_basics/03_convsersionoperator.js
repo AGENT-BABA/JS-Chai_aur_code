212a69efa72409fdf11b36a2e58c9b7c5826e924
Krushna Sonawane <skrushna1983@gmail.com>
2024-10-16 19:15:35 +0000

    conversion

---
+++ 01_basics/03_convsersionoperator.js
@@ -0,0 +1,31 @@
+//{
+// let score=true
+// console.log(typeof score );
+// console.log(typeof (score));
+
+// let numbervalue=(Number)(score)
+
+// console.log(typeof numbervalue);
+// console.log(numbervalue);//bcz it is not a pure number
+
+//"33" => 33
+//"33abc" => NaN
+//true=1 / false=0
+//}
+
+//{
+// let isLoggedIn=1;
+
+// let islogboolean=Boolean(isLoggedIn)
+
+// console.log(islogboolean);
+
+
+// //1 =>true / 0 => false
+// // ""=>false / "bfusddfas" => true
+//}
+
+let somenumber=42
+let numtostring=String(somenumber)
+console.log(numtostring);
+console.log(typeof numtostring);
