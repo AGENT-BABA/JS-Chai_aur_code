d457373e5089be3e73508882cac349284e8b467d
Krushna Sonawane <skrushna1983@gmail.com>
2024-10-16 19:31:26 +0000

    operators

---
+++ 01_basics/04_operators.js
@@ -0,0 +1,38 @@
+// //{
+// let value=43
+// let negvalue=-value
+// console.log(negvalue);
+
+// let str1="Hi"
+// let str2=" Krushna"
+
+// let str=str1+ str2
+// console.log(str);
+// //}
+
+// //{
+// console.log("2" + 2);
+// console.log(2 + "3" + 4)
+// console.log("2" + 3 + 4)
+// console.log( 2+ 3 +"4")
+
+// console.log((2+3)*5 % 5);
+// //}
+// //{
+// console.log(+true);
+// console.log(+"");
+// //}
+
+let num1,num2,num3
+
+num1=num2=num3=2 + 4
+
+console.log(num1);
+
+let sum=100;
+//++sum;
+sum++;
+console.log(sum);
+//prefix or postfix study
+//************************IMP****************/
+
