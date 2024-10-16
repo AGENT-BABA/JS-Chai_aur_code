3781e0da12df50fd0834dc28275d0fd7f3a53f96
Krushna Sonawane <skrushna1983@gmail.com>
2024-10-16 18:33:20 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 933279
+let accountName="Krushna"
+var accountemail="skrushna2006@gmail.com"
+accpass="Agent#007"
+accountName="Swami"
+accountemail="ktackle07@gail.com"
+let accountState
+//const can't be changed.
+// accountId=87849.
+
+/*
+Don't use var bcz of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountName,accountemail,accpass,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//use all JS code as newer version 
+
+//alert (3+3) //we are using node.js not browser
+
+
+console.log("Sum of 3 & 3 is")
+console.log(3
+    +3)//dont write like this bcz it destroys the readability
+
+let name = "Krushna"
+let age=19
+let islogin=false
+
+//num >= 2 to the power 53
+//bigInt
+//string => " "
+// boolean=>true / false
+//null => stand alone value
+//undefined =>
+//symbol => uniquiness
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
