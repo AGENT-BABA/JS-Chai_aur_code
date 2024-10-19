00d5a585efefd9d23a2259cbe41756c8890511a9
Krushna Sonawane <skrushna1983@gmail.com>
2024-10-19 12:52:41 +0000

    all datatypes

---
+++ 01_basics/comparison.js
@@ -0,0 +1,13 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// === is checks strictly the value and type of it
+
+console.log("2"==2);
+console.log("2"===2);
+
---
+++ 01_basics/datatypes_summ.js
@@ -0,0 +1,37 @@
+//Primitive datatypes
+
+//Number,Boolean,String,Symbol,BigInt,null,undefined
+
+
+const score=100;
+const scorevalue=100.8;
+
+const IsLoggedin= false
+const outsidetemp=null
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+console.log(id == anotherid)
+
+const BigNum=39469578439354233n
+console.log(typeof anotherid)
+console.log(typeof BigNum)
+//Reference (Non primitive)
+
+//Array,Objects,Functions
+const heros=["shaktiman",
+    "naagraj","doga"
+]
+
+const myObj=
+{
+    name:"Krushna",
+    age:19,
+}
+
+const myfunction=function()
+{
+    console.log("Hello World!")
+}
+console.log(typeof myfunction)
+console.log(typeof myfunctionkflsd)
