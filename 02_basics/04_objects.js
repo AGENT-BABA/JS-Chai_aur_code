231281914b37bc515f7c6cf9ce6c0acc51e70601
Krushna Sonawane <skrushna1983@gmail.com>
2024-10-23 14:24:22 +0000

    objects over

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,8 +20,46 @@ const regularUser = {
 const obj1 ={1: "a",2: "b"}
 const obj2 ={3: "a",4: "b"}
 
-// const obj3 = {obj1,obj2}
-const obj3 =Object.assign({},obj1,obj2)
+// const obj3 = { obj1 , obj2 }
+
+const obj3 = Object.assign({},obj1,obj2)
+// const obj10 = Object.assign( ...obj1, ...obj2 )
 //use {} this is not necessary but using these is 
+// console.log(obj10)
 console.log(obj3)
+
+const users = [
+    {
+        id:1,
+        email:"Krushna@jh.njinv"
+    },
+    {
+        id:2,
+        email:"Krushna2@jh.njinv"
+    },
+    {
+        id:3,
+        email:"Krushna3@jh.njinv"
+    },
+]
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogIn'))
+
+const course = {
+    coursename : "Krushna",
+    price:"100$",
+    courseinstructor: "Swami"
+}
+
+//course.courseInstructor
+
+const{courseinstructor:Instructor} = course
+
+console.log(courseinstructor)
 
